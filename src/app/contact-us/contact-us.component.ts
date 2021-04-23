4af30121589437f090774bf8317caf7a72aa8881
Daniel Perusse <daniel.p.perusse@gmail.com>
2021-04-23 03:10:11 -0400

    Final commit

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -11,9 +11,24 @@ export class ContactUsComponent implements OnInit {
   @Input() submit: boolean = false;
   @Output() toScreen: EventEmitter<Boolean> = new EventEmitter();
 
+  public name: String;
+  public email: String;
+  public message: String;
+  public error: String;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  onSubmit(inForm: NgForm): void {
+    this.name = inForm.value.userName;
+    this.email = inForm.value.email;
+    this.message = inForm.value.message;
+    if(this.name == null || this.email == null || this.message == null || this.name == "" || this.email == "" || this.message == "") {
+      this.error = "There is an empty field, please fill it";
+    } else {
+      this.error = "";
+    }
+  }
 }
